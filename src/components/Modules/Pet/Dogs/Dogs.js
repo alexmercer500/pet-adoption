c47bf7a73fd0dc8ad2a27ba91e25a15ff755baa0
Ratnesh <ratnesh.kratos1994@gmail.com>
2022-12-10 19:50:03 +0530

    New changes

--- src/components/Modules/Pet/Dogs/Dogs.js
+++ src/components/Modules/Pet/Dogs/Dogs.js
@@ -1,19 +1,21 @@
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import './dogs.css'
 import allData from '../../../Global/Data'
 import Searchbox from '../../main/searchbox/Searchbox'
-
+import searchFilter from '../../../utils/context/searchFunction'
 const Dogs = () => {
 
     const [dogData] = useState(allData.dogs)
+    const { data } = useContext(searchFilter)
 
     return (
         <div>
             <Searchbox />
             <section className='canine-section grid md:grid-cols-2 px-4 lg:grid-cols-3 gap-4 mx-auto p-5'>
-
-                {dogData.map(dog => {
-                    return <div className='canine-cards rounded-md p-2' key={dog.id}>
+                {dogData.filter((dDog) =>
+                    dDog.name.toLowerCase().includes(data)
+                ).map(dog =>
+                    <div className='canine-cards rounded-md p-2' key={dog.id}>
                         <div className='max-h-64 overflow-hidden rounded-t-md'>
                             <img src={dog.photo} alt={dog.name} /></div>
                         <div>
@@ -25,7 +27,7 @@ const Dogs = () => {
                             <button className='mt-5 w-fit'>Get More Info</button>
                         </div>
                     </div>
-                })}
+                )}
             </section>
         </div>
     )
